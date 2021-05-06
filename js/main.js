ba0834baa4003943773df00227b4095e3849c0b0
CesarArellano <raywayday@gmail.com>
2021-05-06 13:05:44 -0500

    Adding buttons and material section

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const row = document.body.querySelector('.row');
+
+const createCards = () => {
+  let i;
+  for(i = 1; i < 10; i++) {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.classList.add('hoverable');
+    div.innerHTML = `
+      <div class="card-image">
+        <img src="https://serviciostecnicosmovil.com/wp-content/uploads/2019/09/electronica.jpg" alt="">
+      </div>
+      <div class="card-details">
+        <h3>Categoria</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, dolorum. Consectetur nemo iusto dolor inventore dignissimos vitae, aspernatur est, nulla a odio temporibus, minus placeat non qui dolores fugiat quas.</p>
+        <a class="material-button" href="material.html?category=${i}">Detalles<i class="iconButton fa fa-arrow-right"></i></a>
+      </div>
+    `;
+    row.append(div);
+  }
+  
+
+};
+
+createCards()
